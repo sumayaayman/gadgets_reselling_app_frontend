b87ebca34fe90a0ce3cb461a9aa5a22e25440256
sumaya ayman <sumayaayman04@gmail.com>
2025-04-04 20:11:11 +0530

    removed color for chat icon

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -81,7 +81,7 @@ const Product = ({
 							View Details
 						</p>
 						<svg
-							className="w-6 h-6 text-gray-500 dark:text-white cursor-pointer"
+							className="w-6 h-6 text-gray-500 cursor-pointer"
 							aria-hidden="true"
 							xmlns="http://www.w3.org/2000/svg"
 							width="24"
